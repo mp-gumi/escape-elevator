cb67631a5e45bae4723248a7f46e8a6a94fee1ba
Kaneko Yoshiki <basil725woodstock@gmail.com>
2022-03-16 22:39:36 +0900

    説明ページのテキストを追加

---
+++ src/components/Introduction/Explanation/index.tsx
@@ -0,0 +1,11 @@
+/** @jsx jsx */
+/** @jsxRuntime classic */
+import { jsx, css } from "@emotion/react";
+
+export function Explanation() {
+  return (
+    <div>
+      <h2>説明</h2>
+    </div>
+  );
+}
--- src/components/Introduction/index.tsx
+++ src/components/Introduction/index.tsx
@@ -1,7 +1,25 @@
 /** @jsx jsx */
 /** @jsxRuntime classic */
 import { jsx, css } from "@emotion/react";
+import { Epilogue } from "./Epilogue";
+import { Explanation } from "./Explanation";
+import { useNavigate } from "react-router-dom";
+
+const startButtonStyle = css`
+  border: 1px solid #000;
+  width: 160px;
+`;
 
 export function Introduction() {
-  return <div></div>;
+  const navigate = useNavigate();
+
+  return (
+    <div>
+      <Epilogue />
+      <Explanation />
+      <div css={startButtonStyle} onClick={() => navigate("/elevator")}>
+        謎解きを始める
+      </div>
+    </div>
+  );
 }
