80cb6450508e105f1f49179d91be1e43c7219906
Kaneko Yoshiki <basil725woodstock@gmail.com>
2022-03-16 19:21:17 +0900

    initial commit

---
+++ src/components/FloorButton/index.tsx
@@ -0,0 +1,8 @@
+/** @jsx jsx */
+import { jsx, css } from "@emotion/react";
+
+function FloorButton() {
+  return <div></div>;
+}
+
+export default FloorButton;
---
+++ src/components/Header/index.tsx
@@ -0,0 +1,8 @@
+/** @jsx jsx */
+import { jsx, css } from "@emotion/react";
+
+function Header() {
+  return;
+}
+
+export default Header;
