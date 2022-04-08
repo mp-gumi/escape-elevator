6496d60a3e6312f5b1abf91b79d3cb19921edd04
Kaneko Yoshiki <basil725woodstock@gmail.com>
2022-04-08 23:39:08 +0900

    クリア後に直リンクした際にリダイレクトするように修正

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,15 +1,29 @@
-import { Routes as Router, Route, BrowserRouter } from "react-router-dom";
+import { useContext } from "react";
+import {
+  Routes as Router,
+  Route,
+  BrowserRouter,
+  Navigate,
+} from "react-router-dom";
 import { Clear } from "../components/Clear";
 import { Introduction } from "../components/Introduction";
 import { Questions } from "../components/Questions";
+import { IsClearedContext } from "../contexts/isClearedContext";
 
 export function Routes() {
+  const { isCleared } = useContext(IsClearedContext);
+
   return (
     <BrowserRouter>
       <Router>
         <Route path="/" element={<Introduction />} />
         <Route path="/elevator" element={<Questions />} />
-        <Route path="/escaped" element={<Clear />} />
+        <Route
+          path="/escaped"
+          element={
+            isCleared.b10fIsCleared ? <Clear /> : <Navigate replace to="/" />
+          }
+        />
       </Router>
     </BrowserRouter>
   );
